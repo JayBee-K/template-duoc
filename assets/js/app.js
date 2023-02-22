bff830bd06fef7cff023b34d3842f717d4cd11fe
Jay <killerdrug007@gmail.com>
2023-02-22 20:24:02 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,15 +25,33 @@
 		}
 	}
 
+	const handleSliderPartner = function () {
+		if ($('#slider-partner').length) {
+			new Swiper('#slider-partner .swiper', {
+				speed: 400,
+				slidesPerView: 6,
+				spaceBetween: 20,
+				preloadImages: false,
+				loop: true,
+				autoplay: {
+					delay: 8000,
+					disableOnInteraction: false,
+				},
+			});
+		}
+	}
+
 	const handleToggleSearch = function () {
 		$('.header-toggle_button').click(function () {
-			let header_search  = $('.header-toggle_button').closest('.header-search');
+			let header_search = $('.header-toggle_button').closest('.header-search');
 			header_search.toggleClass('header-search_show');
 		});
 	}
 
 	$(function () {
 		handleSliderHero();
+		handleSliderPartner();
+
 		handleToggleSearch();
 		$(window).resize(() => {
 			windowWidth = $(window).width();
